f953c2dcfd7bbe3d0967c42c55626d938c138a9b
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-05-08 17:56:08 -0500

    Generando promesa

--- service/client.service.js
+++ service/client.service.js
@@ -1,8 +1,7 @@
-
 //*CREAR ELEMENTO
 //creo el elemento de html que recibira dos parametros de entrada.
 
-const crearNuevaLinea = (nombre,email) => {
+const crearNuevaLinea = (nombre, email) => {
   const linea = document.createElement("tr");
   const contenido = `
    <td class="td" data-td>
@@ -35,21 +34,32 @@ const crearNuevaLinea = (nombre,email) => {
 
 const table = document.querySelector("[data-table]");
 
-const http = new XMLHttpRequest(); //Crear comunicacion entre el Front-end y Back-end
+//*LISTA DE CLIENTES
 
-//Abrir http (metodo,url)
-http.open("GET", "http://localhost:3000/perfil");
+const listaClientes = () => {
+  const promise = new Promise(function (resolve, reject) {
+    //Abrir http (metodo,url)
+    const http = new XMLHttpRequest(); //Crear comunicacion entre el Front-end y Back-end
+    http.open("GET", "http://localhost:3000/perfil");
 
-http.send(); //Se va a encargar de enviar la peticion al navegador o servidor de la url
+    http.send(); //Se va a encargar de enviar la peticion al navegador o servidor de la url
 
-//Ejecutar funcion
-http.onload = () => {
-  const data = JSON.parse(http.response); //transformar el texto con JSON.
-  console.log(data);
-  data.forEach((perfil) => {
-    const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-    table.appendChild(nuevaLinea);
+    http.onload = () => {
+      const response = JSON.parse(http.response); //transformar el texto con JSON. Cambiar respuesta de http para obtner un objeto
+      if(http.status >= 400) {
+        reject(response);
+      } else {
+      resolve(response);
+      }
+    };
   });
+
+  return promise;
 };
 
-console.log(http);
+console.log(listaClientes());
+
+// data.forEach((perfil) => {
+//   const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+//   table.appendChild(nuevaLinea);
+// });

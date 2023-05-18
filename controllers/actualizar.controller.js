5daa762657436de9d41038f24144742514e033d8
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-05-18 17:42:09 -0500

    pantalla de error

--- controllers/actualizar.controller.js
+++ controllers/actualizar.controller.js
@@ -1,8 +1,25 @@
 import { clientServices } from "../service/client-service.js";
-//obtener el identificador del params de la url
+//obtener el identificador del params de la url para entregarlo al html
 
 
 const obtenerInformacion = () => {
    const url = new URL(window.location);
    const id = url.searchParams.get("id");
+
+   //En caso de tener un error mostrar la siguiente pantalla
+   if (id === null) {
+      window.location.href = "/screens/error.html"
+   
+   }
+
+   //situar los datos en el html
+   const nombre = document.querySelector("[data-nombre]")
+   const email = document.querySelector("[data-email]")
+
+   clientServices.detalleCliente(id).then((perfil) => {
+      nombre.value = perfil.nombre;
+      email.valure = perfil.nombre;
+   });
 }
+
+obtenerInformacion();
--- service/client-service.js
+++ service/client-service.js
@@ -31,7 +31,7 @@ const eliminarCliente = (id) => {
 //recibo el id de la url
 
 const detalleCliente = (id) => {
-  return fetch(`http://localhost:3000/perfil/${id}`).then( respuesta => console.log(respuesta));
+  return fetch(`http://localhost:3000/perfil/${id}`).then( respuesta => respuesta.json());
   };
 
 
